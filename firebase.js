853456ef1face091689d38554164015f02365529
Efren Serrano <omar.serrano90@gmail.com>
2022-05-26 18:44:45 -0500

    day 3

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzQrc6BeI73TACPo-oVXM0VgEDuHYSoT8",
+  authDomain: "amzv1-9a160.firebaseapp.com",
+  projectId: "amzv1-9a160",
+  storageBucket: "amzv1-9a160.appspot.com",
+  messagingSenderId: "1028962754750",
+  appId: "1:1028962754750:web:ce04e32462e66b1b2f3eef",
+};
